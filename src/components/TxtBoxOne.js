2dd49e4fad95a712fd148fb90ba73e604a883ad8
Kenneth Luong <102771088+kenluong28@users.noreply.github.com>
2023-03-11 01:38:39 -0500

    about

--- src/components/TxtBoxOne.js
+++ src/components/TxtBoxOne.js
@@ -5,7 +5,10 @@ const TxtBoxOne = () => {
         <div className="text-box">
             <h3>Welcome!</h3>
             <p>
-            I am a student at the University of Waterloo and a candidate for a BASc in Computer Engineering. This is my personal portfolio where I document and share my progression through tech.
+            I am a student at the University of Waterloo and a candidate for a BASc in Computer Engineering.
+            </p>
+            <p>
+            This is my personal portfolio where I document and share my progression through tech.
             </p>
         </div>
     )
--- src/components/TxtBoxTwo.js
+++ src/components/TxtBoxTwo.js
@@ -3,9 +3,30 @@ import React from "react";
 const TxtBoxTwo = () => {
     return (
         <div className="text-box">
-            <h3>Text Box 2</h3>
+            <h3>Who am I?</h3>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                I am an aspiring software/security engineer. I am passionate about Cybersecurity, Security Operations, and Cyber Policies.
+            </p>
+            <p>
+                Studying computer engineering also allows me to have a strong understanding of system design, data structures, and algorithms.
+            </p>
+            <p>
+                As a driven individual, who is eager to progress, I am looking to work in an energetic and performance-driven environment where I can utilize my skills and learn, meanwhile doing so.
+            </p>
+            <p>
+                I am currently working towards completing the CompTIA Security+ and Network+ certifications.
+            </p>
+            <p>
+                Planning on completing the OSCP, GMOB, GXPN, AWS Certified Cloud Practitioner and Certifiied Solutions Architect certifications in the near future.
+            </p>
+            <p>
+                ------------------------------------------------
+            </p>
+            <p>
+                In addition...
+            </p>
+            <p>
+                I enjoy watching movies, reading Japanese literature (manga), playing soccer and video games.
             </p>
         </div>
     )
